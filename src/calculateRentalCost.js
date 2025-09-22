f25c30c6ce7f4eaff5213ac7e59f23e4d4139737
fernanndodev <fernanndo.sillva@gmail.com>
2025-09-22 15:45:09 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,30 @@
+'use strict';
 /**
  * @param {number} days
  *
  * @return {number}
  */
+
+const lowDays = 3;
+const middleDays = 6;
+const withoutDescont = 40;
+const lowDescont = 20;
+const highDescont = 50;
+let total = 0;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days < lowDays) {
+    total = days * withoutDescont;
+
+    return total;
+  } else if (days >= lowDays && days <= middleDays) {
+    total = days * withoutDescont - lowDescont;
+
+    return total;
+  }
+  total = days * withoutDescont - highDescont;
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
